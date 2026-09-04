aa985c3cdc01a258cf918551bfc8d0c5caa5b673
Dawid <dawid.wachecki@onet.pl>
2026-09-04 22:13:07 +0200

    doc for main activity and string values for layouts

--- app/src/main/java/pl/dejwik/myapplication/MainActivity.kt
+++ app/src/main/java/pl/dejwik/myapplication/MainActivity.kt
@@ -1,21 +1,37 @@
 package pl.dejwik.myapplication
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import pl.dejwik.myapplication.databinding.ActivityMainBinding
+import androidx.core.net.toUri
 
-
+/**
+ * Main activity
+ * This activity provides navigation to the card list, game, rules
+ * and information screens.
+ *
+ * @constructor Creates a new MainActivity
+ */
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    /**
+     * On create
+     * Initializes the main screen and configures click listeners
+     * for the available navigation buttons.
+     *
+     * @param savedInstanceState previously saved activity state,
+     * or null if the activity is being created for the first time
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.guzikZKartami.setOnClickListener(){
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://polskieszlagiery.ugu.pl/polskie_szlagiery_karty.pdf"))
+            val browserIntent = Intent(Intent.ACTION_VIEW,
+                "http://polskieszlagiery.ugu.pl/polskie_szlagiery_karty.pdf".toUri())
             startActivity(browserIntent)
         }
 
